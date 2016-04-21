82deee38a50b61786739689eaaa1aca6e1e8b449
Skabold <pierre.linz@orange.fr>
2016-04-21 22:24:45 +0200

    Ajout base mongo, dépendances pom et paramétrage eclipse
    (ce paramétrage ne devrait pas être dans la gestion de conf, mais je
    pense que ce sera plus simple comme cela quand même...)

--- WildlyneDungeon/src/main/java/org/skabold/wildlyne/beans/Item.java
+++ WildlyneDungeon/src/main/java/org/skabold/wildlyne/beans/Item.java
@@ -4,9 +4,30 @@
  */
 package org.skabold.wildlyne.beans;
 
+import org.mongodb.morphia.annotations.Entity;
+
 /**
  * The Class Item.
  */
-public class Item {
+@Entity
+public class Item extends BaseBean {
 
+    /** nom de l'item. */
+    private String nom;
+
+    /**
+     * Gets the nom de l'item.
+     * @return the nom de l'item
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Sets the nom de l'item.
+     * @param nom the new nom de l'item
+     */
+    public void setNom(final String nom) {
+        this.nom = nom;
+    }
 }
